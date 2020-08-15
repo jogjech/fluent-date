61c1a8b4bedded9a344cda5200441bee4404a7a2
Kangxu Wang <kangxu@umich.edu>
2020-08-15 00:15:42 -0700

    Add Chinese support

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 interface Config {
   thresholds: [number, string, string][];
-  smallerThanScalePhrase: string;
+  nowPhrase: string;
   pastPhraseFormat: (quantity: number, unit: string) => string;
   futurePhraseFormat: (quantity: number, unit: string) => string;
   durationPhraseFormat: (quantity: number, unit: string) => string;
 }
 
-const config: Config = {
+const enConfig: Config = {
   thresholds: [
     [31536000, "year", "years"],
     [2628000, "month", "months"], // 365 days / 12. This is to avoid showing 12 months
@@ -16,7 +16,7 @@ const config: Config = {
     [60, "minute", "minutes"],
     [1, "second", "seconds"],
   ],
-  smallerThanScalePhrase: `just now`,
+  nowPhrase: `just now`,
   pastPhraseFormat: (quantity: number, unit: string) => {
     return `${quantity} ${unit} ago`;
   },
@@ -28,29 +28,63 @@ const config: Config = {
   },
 };
 
+const zhConfig: Config = {
+  thresholds: [
+    [31536000, "年", "年"],
+    [2628000, "月", "月"], // 365 days / 12. This is to avoid showing 12 months
+    [604800, "周", "周"],
+    [86400, "天", "天"],
+    [3600, "小时", "小时"],
+    [60, "分钟", "分钟"],
+    [1, "秒", "秒"],
+  ],
+  nowPhrase: `刚刚`,
+  pastPhraseFormat: (quantity: number, unit: string) => {
+    return `${quantity} ${unit}前`;
+  },
+  futurePhraseFormat: (quantity: number, unit: string) => {
+    return `${quantity} ${unit}后`;
+  },
+  durationPhraseFormat: (quantity: number, unit: string) => {
+    return `${quantity} ${unit}`;
+  },
+};
+
 enum Language {
-  ZH,
   EN,
-  JP,
+  ZH,
 }
+
+const languageConfigFactory = (language: Language) => {
+  switch (language) {
+    case Language.EN:
+      return enConfig;
+    case Language.ZH:
+      return zhConfig;
+    default:
+      throw new Error(`Unsupported language: ${language}`);
+  }
+};
 
 class FluentDate {
   language: Language;
+  config: Config;
 
   constructor(language?: Language) {
-    this.language = language || Language.EN;
+    this.language = language === undefined ? Language.EN : language;
+    this.config = languageConfigFactory(this.language);
   }
 
   relative(inputDate: number) {
     const now = Date.now();
     const elapsedSeconds = Math.floor((now - inputDate) / 1000);
-    for (const threshold of config.thresholds) {
+    for (const threshold of this.config.thresholds) {
       if (elapsedSeconds < threshold[0]) continue;
       const quantity = Math.floor(elapsedSeconds / threshold[0]);
       const unit = quantity === 1 ? threshold[1] : threshold[2];
-      return config.pastPhraseFormat(quantity, unit);
+      return this.config.pastPhraseFormat(quantity, unit);
     }
-    return config.smallerThanScalePhrase;
+    return this.config.nowPhrase;
   }
 }
 
--- tst/js/test.js
+++ tst/js/test.js
@@ -1,8 +1,10 @@
-const { FluentDate } = require("../../lib/index.js");
+const { FluentDate, Language } = require("../../lib/index.js");
 
-const enFd = new FluentDate();
-const zhFd = new FluentDate("zh");
+const enFd = new FluentDate(Language.EN);
+const zhFd = new FluentDate(Language.ZH);
 
 console.log(enFd.relative(Date.now() - 60 * 1000 + 1));
+console.log(enFd.relative(Date.now()));
 
 console.log(zhFd.relative(Date.now() - 7200 * 1000 + 1));
+console.log(zhFd.relative(Date.now()));
